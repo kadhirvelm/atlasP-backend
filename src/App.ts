9cd3e252a852fcd4c65f86058cfd4e46e3fd56b4
kadhirvelm <kadhirvelm@berkeley.edu>
2018-09-03 13:34:14 -0700

    Allow cross orgin in develop

--- src/App.ts
+++ src/App.ts
@@ -16,6 +16,13 @@ class PureApp {
     constructor() {
       this.app = express();
       this.app.use(bodyParser.json({ strict: true }));
+      if (process.env.NODE_ENV === "development") {
+        this.app.use((req, res, next) => {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+        });
+      }
 
       this.mountDatabase();
     }
