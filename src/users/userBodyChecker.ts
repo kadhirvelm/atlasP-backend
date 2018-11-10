99b1f457696777046c3c0d1db4b439e02a665696
Kadhir <kadhirvelm@gmail.com>
2018-11-10 15:38:25 -0800

    Hot fix updating (#39)

--- src/users/userBodyChecker.ts
+++ src/users/userBodyChecker.ts
@@ -8,36 +8,62 @@ import {
   isValidMongoID,
   isValidMongoIDArray,
   isValidPhoneNumber,
-  isValidStringArray,
+  isValidStringArray
 } from "../utils";
 import { requiredUserKeys, validGenders } from "./userConstants";
 
-export function validUserBodyChecker(body: any) {
+export function validUserBodyChecker(
+  body: any,
+  needsToBeDefined: boolean = true
+) {
   const errorMessages = [];
-  if (!isSpecificString(body.gender, validGenders)) {
+  if (
+    !isSpecificString(body.gender, validGenders) &&
+    checkNeedsToBeDefined(body.gender, needsToBeDefined)
+  ) {
     errorMessages.push(`Gender is not M, F, or X: ${body.gender}`);
   }
-  if (!isString(body.location)) {
+  if (
+    !isString(body.location) &&
+    checkNeedsToBeDefined(body.location, needsToBeDefined)
+  ) {
     errorMessages.push(`Location is not a string: ${body.location}`);
   }
-  if (!isString(body.name)) {
+  if (
+    !isString(body.name) &&
+    checkNeedsToBeDefined(body.name, needsToBeDefined)
+  ) {
     errorMessages.push(`Name is not a string: ${body.name}`);
   }
-  if (body.phoneNumber !== "" && !isValidPhoneNumber(body.phoneNumber)) {
+  if (
+    body.phoneNumber !== "" &&
+    !isValidPhoneNumber(body.phoneNumber) &&
+    checkNeedsToBeDefined(body.phoneNumber, needsToBeDefined)
+  ) {
     errorMessages.push(`Phone number is not valid: ${body.phoneNumber}`);
   }
-  if (body.ignoreUsers !== undefined && !isValidMongoIDArray(body.ignoreUsers)) {
-    errorMessages.push(`Ignore users must be an array of valid IDs: ${body.ignoreUsers}`);
+  if (
+    body.ignoreUsers !== undefined &&
+    !isValidMongoIDArray(body.ignoreUsers) &&
+    checkNeedsToBeDefined(body.ignoreUsers, needsToBeDefined)
+  ) {
+    errorMessages.push(
+      `Ignore users must be an array of valid IDs: ${body.ignoreUsers}`
+    );
   }
   return errorMessages;
+}
+
+function checkNeedsToBeDefined(item: any, needsToBeDefined: boolean) {
+  return needsToBeDefined || item !== undefined;
 }
 
 export function isValidUser(body: any) {
   const errorMessages = hasCorrectKeys(
     body,
     requiredUserKeys.filter(
-      (key) => key !== "password" && key !== "temporaryPassword",
-    ),
+      key => key !== "password" && key !== "temporaryPassword"
+    )
   );
   if (errorMessages.length > 0) {
     return errorMessages;
@@ -46,20 +72,20 @@ export function isValidUser(body: any) {
 }
 
 export function isValidUserUpdate(body: any, currentUserId: mongo.ObjectId) {
-  const errorMessages = hasCorrectKeys(body, requiredUserKeys);
-  if (!isValidStringArray(Object.values(body))) {
-    errorMessages.push("Cannot leave fields blank.");
-  }
-  if (body.ignoreUsers !== undefined && body.ignoreUsers.includes(currentUserId.toHexString())) {
+  const errorMessages = [];
+  if (
+    body.ignoreUsers !== undefined &&
+    body.ignoreUsers.includes(currentUserId.toHexString())
+  ) {
     errorMessages.push("Cannot ignore yourself.");
   }
-  return validUserBodyChecker(body);
+  return validUserBodyChecker(body, false).concat(errorMessages);
 }
 
 export function isValidLogin(body: any) {
   return (
-    isValidPhoneNumber(body.phoneNumber)
-    && (isString(body.password) || isNumber(body.temporaryPassword))
+    isValidPhoneNumber(body.phoneNumber) &&
+    (isString(body.password) || isNumber(body.temporaryPassword))
   );
 }
 
